d86fa1f790d5cf5ef7822db6d951edc2eda0b354
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2022-04-22 15:06:59 +0200

    Refactor, use only sockjs-client strategy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.css';
 import Layout from './Layout';
-// import StompClient from "react-stomp-client";
-// import {IMessage} from "@stomp/stompjs";
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
-// import io from 'socket.io-client';
 
 
 const websocketURL: string = process.env.REACT_APP_WEB_SOCKET_URL as string;
@@ -13,43 +10,9 @@ const websocketURL: string = process.env.REACT_APP_WEB_SOCKET_URL as string;
 
 function App() {
 
-  // * Za so react-stomp-client
-  // const onMessageReceived = (msg: any) => {
-  // const onMessageReceived = (msg: IMessage) => {
-  //   console.log("=== Websocket Message Received", msg);
-  // }
 
 
-  // * Za so sockjs-client ===> Ne prima messages
-  // let sockJSClient = new SockJS(websocketURL);     // ? Mislam deka treba plus da ima /topic/group
-
-  // sockJSClient.onopen = function() {
-  //   console.log('open');
-  //   sockJSClient.send('test');
-  // };
-
-  // sockJSClient.onmessage = function(e) {
-  //     console.log('message', e.data);
-  //     sockJSClient.close();
-  // };
-
-  // sockJSClient.onclose = function() {
-  //     console.log('close');
-  // };
-
-  // * Za so socket.io-client ===> Blocked by CORS
-  // let sock = io(websocketURL);
-  // sock.on("event://get-message", (msg) => {
-  //   console.log(msg)
-  // })
-
-
-
-
-
-
-
-  // * Za so sockjs-client 2 obid ===> Raboti !!!
+  // * Connect to websocket with sockjs-client 
   let sockJSClient = new SockJS(websocketURL);
 
   let stompClient = Stomp.over(sockJSClient);
@@ -61,8 +24,8 @@ function App() {
 
   stompClient.connect({}, function (frame) {
     console.log('Connected: ' + frame);
-    stompClient.subscribe('/topic/group', function (greeting) {
-      console.log(greeting);
+    stompClient.subscribe('/topic/group', function (message) {     // * Receive messages
+      console.log(message);
       //you can execute any function here
     });
  });
@@ -72,15 +35,6 @@ function App() {
 
   return (
     <>    {console.log(websocketURL)}
-
-      {/* <StompClient
-        endpoint={websocketURL}
-        topic="/topic/group"
-        onMessage={onMessageReceived}
-      >
-        <Layout/>
-      </StompClient> */}
-
 
       <Layout/>
     </>
