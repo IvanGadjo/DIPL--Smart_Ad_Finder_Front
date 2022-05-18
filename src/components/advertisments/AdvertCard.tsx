4f9b2e2df4c104142696af1468554ea64d9fd86c
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2022-05-18 15:59:28 +0200

    byte[] to base64 string, needed to render images from ads

--- src/components/advertisments/AdvertCard.tsx
+++ src/components/advertisments/AdvertCard.tsx
@@ -1,18 +1,42 @@
 import { FC } from "react";
+import { IUserAdvert } from "../../utils/interfaces";
+
+interface IProps {
+    userAdvert: IUserAdvert,         
+}
 
 
 
+const AdvertCard: FC<IProps> = ({ userAdvert }) => {
 
-
-const AdvertCard: FC<{}> = () => {
 
 
     
 
-    return (
+    const arrayBufferToBase64 = ( buffer: any ) => {            // * Convert byte array from backend to base64 endcoded string. Then render <img src=<myBase64String> />
+        var binary = '';
+        var bytes = new Uint8Array( buffer );
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode( bytes[ i ] );
+        }
+        return window.btoa( binary );
+
+    }
+
+
+   
+    
+
+    return(
         <>
 
-                kartica
+
+                
+                    <img alt='нема слика' src={"data:image/png;base64," + arrayBufferToBase64(userAdvert.image)} style={{ width: '250px', height:'250px'}}/>
+                    <br/>
+                    <br/> 
+                
 
         </>
     );
--- src/pages/Advertisments.tsx
+++ src/pages/Advertisments.tsx
@@ -1,14 +1,37 @@
 import { FC } from "react";
 import ActionsPanel from "../components/advertisments/ActionsPanel";
 import AdvertCard from "../components/advertisments/AdvertCard";
+import Loader from "../components/shared/Loader";
+import { IUserAdvert } from "../utils/interfaces";
+import { useUserAdverts } from "../utils/swrHooks/useUserAdverts";
 
 
 const Advertisments: FC<{}> = () => {
 
+    const { allAdverts, isLoadingData } = useUserAdverts();
+
     return (
         <>
-            <ActionsPanel/>
-            <AdvertCard/>
+            {
+                isLoadingData 
+                ? 
+                <>
+                    <Loader/>
+                </> 
+                :
+                <>
+                    <ActionsPanel/>
+                    <br/>
+                    <br/>
+                    
+                    {
+                        allAdverts.map((adv: IUserAdvert) => <AdvertCard userAdvert={adv}/>)
+                    }
+
+                </>
+            }
+
+            
         </>
     )
 }
