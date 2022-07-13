ae67c7041ddc75811b1e58a942177abbd4e201c1
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2022-07-13 14:43:54 +0200

    UI - Ads page actions panel

--- src/components/advertisments/ActionsPanel.tsx
+++ src/components/advertisments/ActionsPanel.tsx
@@ -4,7 +4,16 @@ import { IUserAdvert } from "../../utils/interfaces";
 import { getAllAdverts, getAllAdvertsByUserId } from "../../utils/restServices/userAdvertsService";
 import { useUI_ZustandStore } from '../../utils/zustandStores/userInfoStore';
 import shallow from 'zustand/shallow';
+import { PlusCircleIcon } from "@heroicons/react/outline";
+import { Switch } from "@headlessui/react";
 
+
+function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+
+  
 interface IProps {
     setShownAdverts: Dispatch<SetStateAction<IUserAdvert[] | undefined>>,
     typeOfAdsShown: string,
@@ -39,15 +48,69 @@ const ActionsPanel: FC<IProps> = ({ setShownAdverts, typeOfAdsShown, setTypeOfAd
 
     return (
         <>
-            <Link to='/createUserAdvert'>
-                <button>Ново барање</button>
-            </Link>
-
-            <br/>
-            <br/>
 
 
-            <button onClick={() => handleToggle_advertsShownType()}>Прикажи мои/останати</button>
+            <div className="inline">
+                <Link to='/createUserAdvert'>
+                    <button  
+                    type="button"
+                    className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+                        Нов оглас
+                        <PlusCircleIcon className="ml-2 -mr-1 h-5 w-5" aria-hidden="true" />
+                    </button>
+                </Link>
+            </div>
+
+
+
+
+            {
+                typeOfAdsShown === 'byUser' ? 
+                <button onClick={() => handleToggle_advertsShownType()}
+                className="ml-3 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
+                    Прикажи ги сите
+                </button> 
+                :
+                <button onClick={() => handleToggle_advertsShownType()}
+                className="ml-3 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
+                    Прикажи само мои
+                </button>
+            }
+                
+
+
+            {/* {
+                typeOfAdsShown === 'byUser' ? 
+                <button onClick={() => handleToggle_advertsShownType()}
+                className="block text-sm font-medium text-gray-700 mt-4 mb-2">
+                    Прикажи ги сите
+                </button> 
+                :
+                <button onClick={() => handleToggle_advertsShownType()}
+                className="block text-sm font-medium text-gray-700 mt-4 mb-2">
+                    Прикажи само мои
+                </button>
+            }
+                    
+
+
+            <Switch
+                checked={typeOfAdsShown === 'byUser'}
+                onChange={() => {}}
+                className={classNames(
+                    !(typeOfAdsShown === 'byUser') ? 'bg-green-600' : 'bg-gray-200',
+                    'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'
+                )}
+                >
+                <span className="sr-only">Use setting</span>
+                <span
+                    aria-hidden="true"
+                    className={classNames(
+                        !(typeOfAdsShown === 'all') ? 'translate-x-5' : 'translate-x-0',
+                        'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+                    )}
+                />
+            </Switch> */}
 
         </>
     );
