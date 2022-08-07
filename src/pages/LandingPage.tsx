787353dc99d86f22ad2b7592e4ee6f915fb07c72
Ivan Gadjovski <ivan.gadzovski@gmail.com>
2022-08-07 19:49:54 +0200

    Remove nav from landing page on small screen

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,6 +5,7 @@ import { FC, Fragment } from "react";
 import LogInButton from "../components/auth/logInButton";
 import { navigation } from "../utils/navigationData";
 import smartAdFinderLogo from '../shared/logo_transparent_crop.png';
+import smartAdFinderLogoGreen from '../shared/logo_transparent_green.png';
 import landingPic from '../shared/landing2.png';
 import Tilt from 'react-parallax-tilt';
 import SignUpButton from "../components/auth/signUpButton";
@@ -64,13 +65,7 @@ const LandingPage: FC<{}> = () => {
                 <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
                   <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                     <div className="px-5 pt-4 flex items-center justify-between">
-                      <div>
-                        <img
-                          className="h-8 w-auto"
-                          src="https://tailwindui.com/img/logos/workflow-mark-teal-500-cyan-600.svg"
-                          alt=""
-                        />
-                      </div>
+                     
                       <div className="-mr-2">
                         <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
                           <span className="sr-only">Close menu</span>
@@ -79,33 +74,14 @@ const LandingPage: FC<{}> = () => {
                       </div>
                     </div>
                     <div className="pt-5 pb-6">
-                      <div className="px-2 space-y-1">
-                        {navigation.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.href}
-                            className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
-                          >
-                            {item.name}
-                          </a>
-                        ))}
-                      </div>
-                      <div className="mt-6 px-5">
-                        <a
-                          href="#"
-                          className="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
-                        >
-                          Start free trial
-                        </a>
-                      </div>
-                      <div className="mt-6 px-5">
-                        <p className="text-center text-base font-medium text-gray-500">
-                          Existing customer?{' '}
-                          <a href="#" className="text-gray-900 hover:underline">
-                            Login
-                          </a>
-                        </p>
-                      </div>
+                      
+                      <span className="ml-10">
+                        <SignUpButton/>
+                      </span>
+
+                      <span className="ml-2">
+                        <LogInButton/>
+                      </span>
                     </div>
                   </div>
                 </Popover.Panel>
@@ -149,7 +125,7 @@ const LandingPage: FC<{}> = () => {
                         <Tilt transitionSpeed={200} >
                           {/* <div style={{ backgroundColor: 'darkgreen'}}> */}
                             <img
-                              className="h-36 pt-4 mx-9 w-auto"
+                              className="h-36 pt-4 lg:mx-9 w-auto"
                               src={smartAdFinderLogo}
                               alt="smartAdFinderLogo"
                             />
